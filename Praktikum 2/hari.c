3017a600aa39451959d849d63512dc5600daa60c
Aly <100826322+Visontt@users.noreply.github.com>
2023-03-03 00:07:04 +0700

    Update hari.c

--- Praktikum 2/hari.c
+++ Praktikum 2/hari.c
@@ -6,20 +6,22 @@ int main () {
 printf("Masukan Nomor Hari: ");
 scanf("%d", &h);
 
-    if (h == 1) {
-        printf("Hari Senin");
-    } else if (h == 2) {
-        printf("Hari Selasa");
-    } else if (h == 3){
-        printf("Hari Rabu");
-    } else if (h == 4) {
-        printf("Hari Kamis");
-    } else if (h == 5) {
-        printf("Hari Jum'at");
-    } else if (h == 6) {
-        printf("Hari Sabtu");
-    } else if (h == 7) {
-        printf("Hari Minggu");
+    if (h >= 1 && h <=7) {
+        if (h == 1) {
+            printf("Hari Senin");
+        } else if (h == 2) {
+            printf("Hari Selasa");
+        } else if (h == 3){
+            printf("Hari Rabu");
+        } else if (h == 4) {
+            printf("Hari Kamis");
+        } else if (h == 5) {
+            printf("Hari Jum'at");
+        } else if (h == 6) {
+            printf("Hari Sabtu");
+        } else if (h == 7) {
+            printf("Hari Minggu");
+        }
     } else {
         printf("Masukan nomor hari tidak tepat");
     }
