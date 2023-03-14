d976071c44f9cdb89fc32a39a7f949606a99df58
Aly <100826322+Visontt@users.noreply.github.com>
2023-03-14 19:54:22 +0700

    Update bilangan_prima.c

--- Praktikum 3/bilangan_prima.c
+++ Praktikum 3/bilangan_prima.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+// Nama File : bilangan_prima.c 
+// Deskripsi : Mengidentifikasi jenis suatu bilangan yang mana bilangan tersebut adalah bilangan prima atau lainnya.
+// Pembuat  : Daffa Aly Meganendra - 24060122140140
 
 int main () {
+
+    //Kamus
     int N;
     int faktor;
 
     printf("Masukan input N: ");
     scanf("%d", &N);
 
-
+    //Algoritma
     for (int i = 1; i <= N; i ++) {
         if (N % i == 0) {
             faktor += i;
