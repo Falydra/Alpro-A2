0c855ff2856efe529fa249e15b1ed8b7734b7e08
Aly <100826322+Visontt@users.noreply.github.com>
2023-03-14 20:18:39 +0700

    Update deret_bilangan.c

--- Praktikum 3/deret_bilangan.c
+++ Praktikum 3/deret_bilangan.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+// Nama File : deret_bilangan.c 
+// Deskripsi : Menampilkan bilangan sampai ke-n.
+// Pembuat  : Daffa Aly Meganendra - 24060122140140
 
 int main (void) {
+    //Kamus
     
     int n, total = 0;
     printf("Input n : ");
 
     scanf("%d", &n);
+    
+    //Algoritma
 
     for (int i = 1; i <= n; i++)
     {
