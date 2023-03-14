437defbea183d54a2bcd6a709c849b19e763ab06
Aly <100826322+Visontt@users.noreply.github.com>
2023-03-14 20:19:52 +0700

    Update faktor_bil.c

--- Praktikum 3/faktor_bil.c
+++ Praktikum 3/faktor_bil.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-
+// Nama File : faktor_bil.c 
+// Deskripsi : Menampilkan bilangan yang termasuk faktor dari input n.
+// Pembuat  : Daffa Aly Meganendra - 24060122140140
 
 int main (void) {
+    
+    //Kamus
 
     int N;
 
 
     printf("Masukan input N: ");
     scanf("%d", &N);
+    
+    //Algritma
 
     for (int i = 1; i <= N; i++) {
         if (N % i == 0) {
