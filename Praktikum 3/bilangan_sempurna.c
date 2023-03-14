3163dda82ea465ba67fce3d0c9688857fcbb092f
Aly <100826322+Visontt@users.noreply.github.com>
2023-03-14 20:17:45 +0700

    Update bilangan_sempurna.c

--- Praktikum 3/bilangan_sempurna.c
+++ Praktikum 3/bilangan_sempurna.c
@@ -4,11 +4,14 @@
 // Pembuat  : Daffa Aly Meganendra - 24060122140140
 
 int main () {
+    //Kamus
     int N;
 
     int faktor;
     printf("Masukan input N: ");
     scanf("%d", &N);
+    
+    //Algoritma
 
     for (int i = 1; i < N; i++) {
         if (N % i == 0) {
